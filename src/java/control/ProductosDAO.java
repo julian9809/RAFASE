f80d066da875c48be8f7fbe826a3604664bf96ce
julian9809 <juliand9809@gmail.com>
2020-06-27 12:42:28 -0500

    productos

---
+++ src/java/control/ProductosDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import modelo.Producto;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author julia
+ */
+public class ProductosDAO {
+    
+}
---
+++ src/java/modelo/Producto.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author julia
+ */
+public class Producto {
+    private double id_producto;
+    private String nombre_producto;
+    private String marca_producto;
+    private String referencia_producto;
+    private String caracteristicas_producto;
+    private String foto;
+    private double precio_base;
+    private String unidad_medida;
+    private double id_subcategoria;
+    
+    
+
+    public double getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(double id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public String getMarca_producto() {
+        return marca_producto;
+    }
+
+    public void setMarca_producto(String marca_producto) {
+        this.marca_producto = marca_producto;
+    }
+
+    public String getReferencia_producto() {
+        return referencia_producto;
+    }
+
+    public void setReferencia_producto(String referencia_producto) {
+        this.referencia_producto = referencia_producto;
+    }
+
+    public String getCaracteristicas_producto() {
+        return caracteristicas_producto;
+    }
+
+    public void setCaracteristicas_producto(String caracteristicas_producto) {
+        this.caracteristicas_producto = caracteristicas_producto;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public double getPrecio_base() {
+        return precio_base;
+    }
+
+    public void setPrecio_base(double precio_base) {
+        this.precio_base = precio_base;
+    }
+
+    public String getUnidad_medida() {
+        return unidad_medida;
+    }
+
+    public void setUnidad_medida(String unidad_medida) {
+        this.unidad_medida = unidad_medida;
+    }
+
+    public double getId_subcategoria() {
+        return id_subcategoria;
+    }
+
+    public void setId_subcategoria(double id_subcategoria) {
+        this.id_subcategoria = id_subcategoria;
+    }    
+    
+}
+
+
