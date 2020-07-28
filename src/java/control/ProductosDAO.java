20d99622f80ce314ee76465b7d825fd932c44082
SantiYami <davidgarces.98@gmail.com>
2020-07-27 19:57:08 -0500

    vista inventario_rafase

--- src/java/control/ProductosDAO.java
+++ src/java/control/ProductosDAO.java
@@ -29,15 +29,25 @@ public class ProductosDAO {
      * @param usuario
      * @param password
      * @param producto_buscado
+     * @param subcategoria_buscada
+     * @param categoria_buscada
      * @throws CaException
      */
-    public void buscarProducto(String usuario, String password, String producto_buscado) throws CaException {
+    public void buscarProducto(String usuario, String password,
+            String producto_buscado, String subcategoria_buscada,
+            String categoria_buscada) throws CaException {
         try {
-            String strSQL = "SELECT id_producto,nombre_producto,marca_producto,"
+            String strSQL = "SELECT * FROM INVENTARIO_RAFASE "
+                    + "WHERE ID_CIUDAD = ciudad "
+                    + "AND NOMBRE_PRODUCTO LIKE UPPER('%"+producto_buscado+"%') "
+                    + "AND ID_SUBCATEGORIA LIKE UPPER('%"+subcategoria_buscada+"%') "
+                    + "AND ID_CATEGORIA LIKE UPPER('%"+categoria_buscada+"%') "
+                    + "ORDER BY NOMBRE_PRODUCTO ASC";
+                    /*"SELECT id_producto,nombre_producto,marca_producto,"
                     + "referencia_producto,caracteristicas_producto,foto,id_subcategoria,"
                     + "iva,unidad_medida FROM prod WHERE "
                     + "UPPER(nombre_producto) LIKE UPPER('%" + producto_buscado
-                    + "%')";
+                    + "%')";*/
             Connection conexion = ServiceLocator.getInstance(usuario, password).tomarConexion();
             try (PreparedStatement prepStmt = conexion.prepareStatement(strSQL)) {
                 ResultSet rs = prepStmt.executeQuery();
